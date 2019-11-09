137fdd76486f28909749758090a612bf55947753
Mohamed Fathy <codetwostart@gmail.com>
2019-11-09 07:35:40 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace introtoasynmethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Staring in Main method");
+            Run();
+            Console.WriteLine("Ending in Main method");
+        }
+        static async void Run()
+        {
+            Task<int> r = Worker();
+            await r;
+            Console.WriteLine("The result is " + r.Result);
+        }
+        static Task<int> Worker()
+        {
+            return Task<int>.Factory.StartNew(() =>
+            {
+                int counter = 0;
+                for (int i = 0; i < 100000; i++)
+                {
+                    counter += i;
+                }
+                return counter;
+            });
+        }
+    }
+}
